a15565287605a386993d698a607979cc82c961ea
Van Pho Nguyen <vanpho02@gmail.com>
2018-03-05 18:45:55 +0700

    add.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const reload = require('reload');
+const parser = require('body-parser').urlencoded({ extended: false });
+
 const { Singer, singers } = require('./Singer');
 
 const app = express();
@@ -15,6 +17,13 @@ app.get('/remove/:id', (req, res) => {
     const index = singers.findIndex(singer => singer.id === +id);
     if (index === -1) return res.send('Khong tim thay ca si.');
     singers.splice(index, 1);
+    res.redirect('/');
+});
+
+app.post('/add', parser, (req, res) => {
+    const { name, link, image } = req.body;
+    const singer = new Singer(name, link, image);
+    singers.push(singer);
     res.redirect('/');
 });
 
