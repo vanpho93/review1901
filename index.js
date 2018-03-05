05b7d8a4680ec584009b529a62ea77c7e019cc74
Van Pho Nguyen <vanpho02@gmail.com>
2018-03-05 18:23:13 +0700

    route remove

--- index.js
+++ index.js
@@ -10,5 +10,15 @@ app.get('/', (req, res) => {
     res.render('index', { singers });
 });
 
+app.get('/remove/:id', (req, res) => {
+    const { id } = req.params;
+    const index = singers.findIndex(singer => singer.id === +id);
+    if (index === -1) return res.send('Khong tim thay ca si.');
+    singers.splice(index, 1);
+    res.redirect('/');
+});
+
+app.get('/add', (req, res) => res.render('add'));
+
 app.listen(3000, () => console.log('Server started!'));
 reload(app);
